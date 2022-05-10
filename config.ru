a551dcce8a3990d2346f28f4f79a7bde0ec6b773
Grzegorz Smajdor <grzegorz@yelp.com>
2022-05-10 17:48:18 +0200

    Configure app

--- config.ru
+++ config.ru
@@ -1,1 +1,2 @@
-
+require './my_app'
+run MyApp
--- my_app.rb
+++ my_app.rb
@@ -2,11 +2,17 @@
 # myapp.rb
 require 'bundler'
 Bundler.require
+require 'sinatra/base'
 require 'sinatra/json'
 
-get '/' do
-  query = BasicYahooFinance::Query.new
-  data = query.quotes('YELP')
-  price = data['YELP']['regularMarketPrice']
-  json price: price
+class MyApp < Sinatra::Base
+  get '/' do
+    query = BasicYahooFinance::Query.new
+    data = query.quotes('YELP')
+    price = data['YELP']['regularMarketPrice']
+    json price: price
+  end
+
+
+    run! if app_file == $0
 end
